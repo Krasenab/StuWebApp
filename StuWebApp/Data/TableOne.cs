735f2d0052b5de41acdfe03c1da5c07aa87b4059
krasennikolicov@abv.bg <krasennikolicov@abv.bg>
2024-02-24 15:59:11 +0200

    add commit

---
+++ StuWebApp/Data/TableOne.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StuWebApp.Data
+{
+    public class TableOne
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Position { get; set; }
+    }
+}
